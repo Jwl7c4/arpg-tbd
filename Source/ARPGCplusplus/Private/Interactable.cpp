70eb6c723a49ed940db3051f06a89f81c57acf24
Jake Lowrey <jwl7c4@gmail.com>
2023-09-25 20:54:13 -0500

    dropped loot bp goes into inventory and destorys

---
+++ Source/ARPGCplusplus/Private/Interactable.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactable.h"
+
+bool IInteractable::Interact(AActor* InteractingActor)
+{
+    return false;
+}
